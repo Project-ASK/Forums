86400c90566a98dc1abe534c85a8178268c8ed74
Karthik Vijay <kv52274@gmail.com>
2024-02-09 16:32:25 +0530

    Add onClick event to Login button(Mobile View)

--- Frontend/pages/index.js
+++ Frontend/pages/index.js
@@ -52,7 +52,7 @@ const Test = () => {
           </div>
           {isMobile &&
             <div className="absolute top-4 right-2 flex items-center">
-              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Login</button>
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={Login}>Login</button>
             </div>
           }
           <div className="relative top-6">
