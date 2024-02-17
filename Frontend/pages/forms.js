10eee4666f7616c8a8f3cf3dae4c410eaa7b99b3
R Ashwin <77284634+ashwin417@users.noreply.github.com>
2024-02-17 14:10:15 +0530

    new file:   Frontend/pages/forms.js

---
+++ Frontend/pages/forms.js
@@ -0,0 +1,14 @@
+"use client"
+
+import { useRouter } from "next/router"
+import { useState } from "react"
+
+export default function CreateForm() {
+    return (
+        <main>
+            <h2 className="text-4xl font-bold text-center">Create a Ticket</h2>
+            <div>CreateForm</div>
+        </main>
+        )
+
+}
