ecabb00e1a4adb8d712bbebc703164183f87c669
M3BIONIX <you@example.com>
2024-03-03 10:53:35 +0530

    userForum Editsz

--- Frontend/pages/user/userForum.js
+++ Frontend/pages/user/userForum.js
@@ -9,6 +9,7 @@ export default function userForum() {
     const router = useRouter();
     const [det, setdet] = useState('')
     const [name, setName] = useState('')
+    const [furl, setfurl] = useState('')
     const [side, setSide] = useState('Home')
     const handleClickOutside = e => {
         if (node.current.contains(e.target)) {
@@ -21,29 +22,10 @@ export default function userForum() {
 
     useEffect(() =>{
         setName(router.query.data)
+        getData()
     }, [router.query]);
 
-    useEffect(() =>{
-      fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/forums`, {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({ name }),
-            }).then(response => {
-                if (!response.ok) {
-                  throw new Error('Network response was not ok');
-                }
-                return response.json();
-              })
-              .then(data => setdet(data))
-              .catch(error => {
-                console.error('There has been a problem with your fetch operation:', error);
-              });
-
-        })
-
-    useEffect(() => {
+   useEffect(() => {
         if (open) {
             document.addEventListener("mousedown", handleClickOutside);
         } else {
@@ -54,6 +36,12 @@ export default function userForum() {
             document.removeEventListener("mousedown", handleClickOutside);
         };
     }, [open]);
+
+    async function getData(){
+        const data = require(`../../public/forumDetails/${router.query.data}/details.json`);
+        setdet(data.details);    
+        setfurl(data.url);
+    }
 
     return(
         <>
@@ -71,9 +59,9 @@ export default function userForum() {
                 <img src="/assets/home.svg" className="ml-[30px]" width={20}/>
                 <p className="font-sans text-md">Home</p>
             </div>
-            <div className="flex space-x-5 items-center p-3 hover:bg-gray-100 hover:transition-colors hover:ease-in-out hover:duration-500 rounded-lg">
+            <div className="flex space-x-5 items-center p-3 hover:bg-gray-100 hover:transition-colors hover:ease-in-out hover:duration-500 rounded-lg" onClick={()=>{setSide('Events')}}>
                 <img src="/assets/home.svg" className="ml-[30px]" width={20}/>
-                <p className="font-sans text-md">Home</p>
+                <p className="font-sans text-md">Events</p>
             </div>
        
             
@@ -101,23 +89,25 @@ export default function userForum() {
               {/* Hero content */}
               <div className="pt-32 pb-12 md:pt-40 md:pb-20">
                 {/* Section header */}
-                <div className="text-center pb-12 md:pb-16">
+                <div className="text-center pb-12 md:pb-16 space-y-8">
                   <h1 className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4" data-aos="zoom-y-out">
                     {name} <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">CEC</span>
                   </h1>
-                  <div className="max-w-3xl mx-auto">
-                    <p className="text-xl text-gray-600 mb-8" data-aos="zoom-y-out" data-aos-delay="150">
-                     {console.log(det)}
+                  <div className="mx-auto max-w-3xl">
+                    <p className="text-xl text-gray-600 mb-8 text-justify" data-aos="zoom-y-out" data-aos-delay="150">
+                     {det}
                     </p>
-                    <div className="max-w-xs mx-auto space-x-5 flex sm:justify-center flex-row" data-aos="zoom-y-out" data-aos-delay="300">
+                    <div className="mx-auto max-w-sm space-x-5 flex justify-center flex-row" data-aos="zoom-y-out" data-aos-delay="300">
                       <div>
-                        <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="#0">
+                        <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" onClick={()=>{setSide("Events")}}>
                             View Events
                         </a>
                       </div>
                       <div>
-                        <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">
-                          Learn more
+                       
+                        <a className="btn items-center space-x-3 text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" onClick={()=>{router.push(furl)}}>
+                        <img src="/assets/web.png" width={20} />
+                          <p>Website</p>
                         </a>
                       </div>
                     </div>
@@ -148,16 +138,8 @@ export default function userForum() {
                             <stop stopColor="#EAEAEA" offset="48.57%" />
                             <stop stopColor="#DFDFDF" stopOpacity="0" offset="100%" />
                           </linearGradient>
-                          <radialGradient cx="21.152%" cy="86.063%" fx="21.152%" fy="86.063%" r="79.941%" id="hero-ill-e">
-                            <stop stopColor="#4FD1C5" offset="0%" />
-                            <stop stopColor="#81E6D9" offset="25.871%" />
-                            <stop stopColor="#338CF5" offset="100%" />
-                          </radialGradient>
-                          <circle id="hero-ill-d" cx="384" cy="216" r="64" />
                         </defs>
                         <g fill="none" fillRule="evenodd">
-                          <circle fillOpacity=".04" fill="url(#hero-ill-a)" cx="384" cy="216" r="128" />
-                          <circle fillOpacity=".16" fill="url(#hero-ill-b)" cx="384" cy="216" r="96" />
                           <g fillRule="nonzero">
                             <use fill="#000" xlinkHref="#hero-ill-d" />
                             <use fill="url(#hero-ill-e)" xlinkHref="#hero-ill-d" />
@@ -184,14 +166,16 @@ export default function userForum() {
                       </svg>
                       <span className="ml-3">Watch the full video (2 min)</span>
                     </button>
-                  </div>
-      
-                  {/* Modal */}
-                  
+                  </div>                 
                 </div>
               </div>
             </div>
           </section>
+        }
+        {
+            side === "Events" && <div>
+              addEventLi
+            </div>
         }
         
         </>
